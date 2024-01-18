c6cb9b6314547c54caad85d3c08b4794f18800a6
ParsaKargari <parsa.kargari@ucalgary.ca>
2024-01-18 13:34:34 -0700

    SETUP: Added color pallete
    Signed-off-by: ParsaKargari <parsa.kargari@ucalgary.ca>

---
+++ frontend/src/values/colors.js
@@ -0,0 +1,12 @@
+// colors.js
+
+// global color values
+
+export const colors = {
+    "white": "#FFFFFF",
+    "background": "F8FAFC",
+    "color1": "E2E8F0",
+    "primary": "#334155",
+    "secondary": "#00789E",
+    "color2": "#EDFFFF",
+};
