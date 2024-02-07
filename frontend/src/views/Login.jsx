6cea986c5162199c8f531aba60cb779797d5fdd5
ParsaKargari <parsa.kargari@ucalgary.ca>
2024-02-06 19:52:45 -0700

    FIX: .env for API keys
    Signed-off-by: ParsaKargari <parsa.kargari@ucalgary.ca>

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -3,15 +3,13 @@ import { createClient } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Outlet, useNavigate } from "react-router-dom";
-
 import "../css/Login.css";
 
 // this needs to be put in a env file at the end of the project for security.
 const supabase = createClient(
-  "https://opibjtddqpdpnytgulvm.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9waWJqdGRkcXBkcG55dGd1bHZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1MDU1MzUsImV4cCI6MjAyMjA4MTUzNX0.bzwZbig3eS8EiUof8ium4yDVIm607IlGL0xq6vaYiEU"
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
 );
-
 
 function Login() {
   localStorage.clear();
