6367fd00f2aa6c85dcb2ff85b7f394f42bb6b319
grnsingh <gurnoor.singh1@ucalgary.ca>
2024-03-01 16:35:06 -0700

    updated groups, and tasks to get and change stuff

---
+++ frontend/src/Controllers/ApplicationAPIs/Groups.js
@@ -0,0 +1,49 @@
+const SupabaseConnector = require('../APIGateway/Supabase.js');
+// NOT YET TESTED
+class Groups {
+    constructor() {
+        this.supabase = new SupabaseConnector();
+        this.client = this.supabase.getClient();
+    }
+
+    async createGroup(groupName, members, groupStatus, chat, admin) {
+        try {
+            const result = await this.client
+                .from('Groups')
+                .insert([{ GroupName: groupName, Members: members, GroupStatus: groupStatus, Chat: chat, Admin: admin }])
+                .select();
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async getGroups() {
+        try {
+            const result = await this.client
+                .from('Groups')
+                .select('*');
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async getGroupById(groupId) {
+        try {
+            const result = await this.client
+                .from('Groups')
+                .select('*')
+                .eq('GroupID', groupId)
+                .single();
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+}
+
+module.exports = Groups;
--- frontend/src/Controllers/ApplicationAPIs/Tasks.js
+++ frontend/src/Controllers/ApplicationAPIs/Tasks.js
@@ -1,9 +1,67 @@
 const SupabaseConnector = require('../APIGateway/Supabase.js');
+//  TO BE TESTED
+class Tasks {
+    constructor() {
+        this.supabase = new SupabaseConnector();
+        this.client = this.supabase.getClient();
+    }
 
-const addTask = (task) => {
-    const supabase = SupabaseConnector.getInstance();
-    let client = supabase.getClient();
-    return client.from('Tasks').insert([
-        {},
-    ]).select();
+    async createTask(userID, taskDescription, addedDate, dueDate, publicVisibility) {
+        try {
+            const result = await this.client
+                .from('Tasks')
+                .insert([{ UserID: userID, TaskDescription: taskDescription, AddedDate: addedDate, DueDate: dueDate, PublicVisibility: publicVisibility }])
+                .select();
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async getTasks() {
+        try {
+            const result = await this.client
+                .from('Tasks')
+                .select('*');
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async getTaskById(taskId) {
+        try {
+            const result = await this.client
+                .from('Tasks')
+                .select('*')
+                .eq('TaskID', taskId)
+                .single();
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+    async updateTaskById(taskId, columnName, newData) {
+        try {
+            const updateObject = {};
+            updateObject[columnName] = newData;
+
+            const result = await this.client
+                .from('Tasks')
+                .update(updateObject)
+                .eq('TaskID', taskId)
+                .single();
+
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
 }
+
+module.exports = Tasks;
