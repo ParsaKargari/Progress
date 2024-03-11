9ecbebeefcc554f4c4adea99e3b951996963147c
Vishnu Dhanda <vishnu.dhanda@ucalgary.ca>
2024-03-11 10:53:18 -0600

    this is sick

--- frontend/src/components/MyUser.jsx
+++ frontend/src/components/MyUser.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 import CircularProgress from '@mui/joy/CircularProgress';
 import '../css/MusicAnimation.css';
+import Settings from './Settings';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 export default function FriendProfile() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
     return (
         <div className='flex flex-row justify-start items-center py-1.5 pb-3 border-t-2 border-betterWithFriends'>
             <CircularProgress
@@ -12,7 +30,10 @@ export default function FriendProfile() {
                 size="md"
                 value={20}
                 variant="solid"
+                onClick={handleClickOpen}
             />
+
+            
             <div className='flex flex-col align-items-center'>
                 <div className='flex flex-row'>
                     <div className='flex'>
@@ -26,6 +47,36 @@ export default function FriendProfile() {
                     </div>
                 </div>
             </div>
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                PaperProps={{
+                    style: {
+                    width: '35%',
+                    height: '50%',
+                    padding: '15px',
+                    borderRadius: '20px',
+                    },
+                  }}
+            >
+                <div className='flex-row '>
+                <p className='font-bold text-DarkGrey font-standard text-[20px] ml-3 mr-1'>{'Settings'}</p>
+                
+                    <p className='font-bold text-DarkGrey font-standard text-[16px] ml-3 mr-1'>Username</p>
+                    
+
+                    <p className='font-bold text-DarkGrey font-standard text-[16px] ml-3 mr-1'>Status</p>
+                    <p className='font-bold text-DarkGrey font-standard text-[16px] ml-3 mr-1'>Ring Color</p>
+                    <p className='font-bold text-DarkGrey font-standard text-[16px] ml-3 mr-1'>Connect Spotify</p>
+                </div>
+
+                <Button>Logout</Button>
+            </Dialog>
+
+
         </div>
     );
 }
---
+++ frontend/src/components/Settings.jsx
@@ -0,0 +1,10 @@
+export default function Settings() {
+    return(
+        <div class="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
+        <div class="bg-gray-800 bg-opacity-75 absolute w-1/5 h-1/5 rounded-lg shadow-lg p-4">
+            This is a test
+        </div>
+        </div>
+
+    );
+}
