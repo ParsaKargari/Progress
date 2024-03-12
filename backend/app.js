783d09791bd7f917ba89adba8bb4d7781b7dd50b
dannicklucas <dannicklucas@gmail.com>
2024-03-12 17:31:58 -0600

    removed deleted routes from app.js
    Signed-off-by: dannicklucas <dannicklucas@gmail.com>

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
 var signUpRouter = require('./routes/SignUp')
 var app = express();
 
@@ -20,9 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
+
 app.use('/signUp', signUpRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
