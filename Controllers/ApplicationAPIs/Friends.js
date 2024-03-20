8ef0568579a568f07c45b68e4b387f5220473f9c
thomasbhavnani <thomas.bhavnani@ucalgary.ca>
2024-03-20 10:01:41 -0600

    added more friends function

--- Controllers/ApplicationAPIs/Friends.js
+++ Controllers/ApplicationAPIs/Friends.js
@@ -31,20 +31,60 @@ class Friends {
         }
     }
 
+    async sendFriendRequest(from_id, to_id) {
+        let { data, error } = await this.client
+            .rpc('send_friend_request', {
+                from_id,
+                to_id
+            })
+        if (error) console.error(error)
+        else console.log(data)
+
+    }
+
+    async receiveFriendRequest(from_id, to_id) {
+        let { data, error } = await this.client
+            .rpc('receive_friend_request', {
+                from_id,
+                to_id
+            })
+        if (error) console.error(error)
+        else console.log(data)
+    }
+    async removeFriendRequestReceived(from_id, to_id) {
+        let { data, error } = await this.client
+            .rpc('remove_friend_request', {
+                from_id,
+                to_id,
+            })
+        if (error) console.error(error)
+        else console.log(data)
+    }
+
+    async removeFriendRequestSent(from_id, to_id) {
+        let { data, error } = await this.client
+            .rpc('remove_friend_request_sent', {
+                from_id,
+                to_id,
+            })
+        if (error) console.error(error)
+        else console.log(data)
+    }
+
     async sendAndReceiveFriendRequest(fromID, toID) {
-        const resultSend = await signup.sendFriendRequest(fromID, toID);
-        const resultReceive = await signup.receiveFriendRequest(fromID, toID);
+        const resultSend = await this.sendFriendRequest(fromID, toID);
+        const resultReceive = await this.receiveFriendRequest(fromID, toID);
     }
 
     async acceptFriendRequest(fromID, toID) {
-        const resultRemoveReceive = await signup.removeFriendRequestReceived(fromID, toID);
-        const resultRemoveSend = await signup.removeFriendRequestSent(fromID, toID);
+        const resultRemoveReceive = await this.removeFriendRequestReceived(fromID, toID);
+        const resultRemoveSend = await this.removeFriendRequestSent(fromID, toID);
         const addedFriend = this.addFriend(fromID, toID);
     }
 
     async declineFriendRequest(fromID, toID) {
-        const resultRemoveReceive = await signup.removeFriendRequestReceived(fromID, toID);
-        const resultRemoveSend = await signup.removeFriendRequestSent(fromID, toID);
+        const resultRemoveReceive = await this.removeFriendRequestReceived(fromID, toID);
+        const resultRemoveSend = await this.removeFriendRequestSent(fromID, toID);
     }
 }
 
