8775a104e4156be81ba9386ade1d5b14e5a2e741
ParsaKargari <parsa.kargari@ucalgary.ca>
2024-03-21 16:19:09 -0600

    Update CORS settings to allow all origins

--- backend/app.js
+++ backend/app.js
@@ -10,23 +10,6 @@ var signUpRouter = require("./routes/SignUp");
 var app = express();
 var cors = require('cors');
 
-var allowedOrigins = ['http://localhost:3000', 'https://progresslive.vercel.app'];
-
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
--- backend/routes/SignUp.js
+++ backend/routes/SignUp.js
@@ -2,7 +2,7 @@ const SupabaseConnector = require('../SupabaseConnector.js');
 var express = require('express');
 var router = express.Router();
 router.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
