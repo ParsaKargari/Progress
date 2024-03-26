9176b1c8f1511f8893001768109593185b689d64
dannicklucas <dannicklucas@gmail.com>
2024-03-25 19:17:22 -0600

    add friend working
    Signed-off-by: dannicklucas <dannicklucas@gmail.com>

--- frontend/src/components/IncomingRequest.jsx
+++ frontend/src/components/IncomingRequest.jsx
@@ -1,14 +1,28 @@
-export default function IncomingRequest() {
+import axios from "axios";
+import { useAuth } from "../context/AuthContext";
+export default function IncomingRequest(props) {
+    const  data  = props.name;
+    const { user } = useAuth();
+
+
+    function accept() {
+        axios.get(`${process.env.REACT_APP_API_URL}/friends/acceptFriend/${user.id}/${data}`)
+
+    }
+    function decline() {
+        axios.get(`${process.env.REACT_APP_API_URL}/friends/declineFriend/${user.id}/${data}`)
+
+    }
     return (
         <div className='flex flex-row my-1 justify-between'>
             <div className="font-standard text-DarkGrey text-16">
-                Grinder1
+                {data}
             </div>
             <div className="flex flex-row">
-            <div className="cursor-pointer px-5 text-textcolour font-standard text-16 decoration-2 truncate">
+            <div className="cursor-pointer px-5 text-textcolour font-standard text-16 decoration-2 truncate" onClick={accept}>
                 Accept
             </div>
-            <div className="cursor-pointer text-textcolour font-standard text-16 decoration-2 truncate">
+            <div className="cursor-pointer text-textcolour font-standard text-16 decoration-2 truncate" onClick={decline}>
                 Decline
             </div>
             </div>
