d0204f0c982581828ee57423ed9857cf6ec7cea8
dannicklucas <dannicklucas@gmail.com>
2024-03-26 20:45:10 -0600

    trying to set up chai and mocha
    Signed-off-by: dannicklucas <dannicklucas@gmail.com>

---
+++ backend/Controllers/ApplicationAPITests.js/chaiTests/ChaiTests.js
@@ -0,0 +1,20 @@
+
+
+const assert = require('chai').assert;
+const Chef = require('../../../bin/www');
+
+describe('Chef test', function(){
+   
+    let chef = Chef;
+    
+    it('check the dish has valid name.', function(){
+        assert.isString(chef.checkMenu(), 'string');
+    })
+    
+    it('check for a dish in menu.', function (){
+        let dish= chef.checkMenu()
+        assert.oneOf(dish, chef.dishes)
+
+    });
+    
+});
